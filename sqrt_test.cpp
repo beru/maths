ec864642da716e73336438fdcfd31c5b071cc9a4
beru <berupon@gmail.com>
2015-01-03 21:05:19 +0900

    sqrtの実装を追加

--- sqrt_test.cpp
+++ sqrt_test.cpp
@@ -30,6 +30,7 @@ THE SOFTWARE.
 #include <intrin.h>
 #include <math.h>
 #include <float.h>
+#include <assert.h>
 
 double g_isqrt_initial_values[256+1];
 
@@ -60,21 +61,32 @@ double calc_recipro_sqrt(double s)
 	}
 
 	double x = initial;
-	double s10 = s * 10;
-	double ss3 = s * s * 3;
+#if 0
+	// quartic algorithm
 	for (size_t i=0; i<2; ++i) {
-		double xs = x * x;
-//		double y = s * xs;
-//		x = x / 8 * (15 - y * (10 - 3 * y));
-		x *= (15 - xs * (s10 - ss3 * xs));
-		x /= 8;
+		double h = 1 - s * x * x;
+		double h2 = h * h;
+		double h3 = h * h2;
+		x = x + x * (8 * h + 6 * h2 + 5 * h3) / 16;
+//		x = x + x * h * (8 + h * (6 + 5 * h)) / 16;
 	}
+#elif 0
+	// cubic iteration
+	for (size_t i=0; i<2; ++i) {
+		double h = 1 - s * x * x;
+		double h2 = h * h;
+		x = x + x * (4 * h + 3 * h2) / 8;
+	}
+#else
+	for (size_t i=0; i<4; ++i) {
+		double h = 1 - s * x * x;
+		x = x + x * h / 2;
+	}
+#endif
 	return x;
 }
 
-
-
-int main(int argc, char* argv[])
+void testFloat()
 {
 	// setup table
 	{
@@ -90,13 +102,62 @@ int main(int argc, char* argv[])
 	}
 
 	double begin = 0;
-	double end = 1024.0;
-	double step = (end - begin) / 10240;
+	double end = 64;
+	double step = (end - begin) / 32;
 	for (double x = begin; x < end; x += step) {
 		double recipro_sqrt = calc_recipro_sqrt(x);
-		printf("%f %f %.10f\n", x, x * recipro_sqrt, x*x*recipro_sqrt*recipro_sqrt);
+		double square_root0 = sqrt(x);
+		double square_root1 = x * recipro_sqrt;
+		double square_root2 = 1.0/recipro_sqrt;
+		printf("%f %.13f %.13f\n", x, square_root0, square_root2);
 	}
-	
+}
+
+// 引数 s  : 負でない整数
+// 引数 oq : 戻り値の固定小数点のQ値（小数部ビット数）
+// 戻り値  : 平方根の逆数 固定小数点形式
+uint32_t calc_recipro_sqrt_integer(uint32_t s, size_t* oq)
+{
+	if (s == 0) {
+		return 0;
+	}
+	size_t clz = __lzcnt(s);
+	size_t nBits = (32 - clz) & (~1);
+	s <<= clz;
+	size_t offset = 31 - (clz >> 1);
+	uint32_t x = 1u << 31;
+	for (size_t i=0; i<4; ++i) {
+		uint32_t s_mul_x = ((uint64_t)s * x) >> 31;								// (Q.clz * Q.31) >> 31 = Q.clz
+		uint32_t x_mul_x = ((uint64_t)x * x) >> 31;								// (Q.31 * Q.31) >> 31 = Q.31
+		uint64_t s_mul_x_mul_x_mul_x = ((uint64_t)s_mul_x * x_mul_x) >> (clz + nBits);	// (Q.clz * Q.31) >> (clz + nBits) = Q.(31 - nBits)
+		x = x + ((x - s_mul_x_mul_x_mul_x) >> 1);
+	}
+	*oq = 16 + offset - (clz & 1);
+	return x;
+}
+
+void testFixedPoint()
+{
+
+//	calc_recipro_sqrt_integer(125348);
+
+	uint32_t begin = 1 << 0;
+	uint32_t end = begin + 64;
+	size_t q = 0;
+	for (uint32_t i = begin; i < end; ++i) {
+		size_t q;
+		uint32_t x = calc_recipro_sqrt_integer(i, &q);
+		double square_root0 = sqrt((double)i);
+		double square_root1 = ((uint64_t)i * x) / (double)(1llu << q);
+		double square_root2 = 1.0 / (x / (double)(1llu << q));
+		printf("%u %u %.13f %.13f\n", i, x, square_root0, square_root2);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	testFloat();
+	testFixedPoint();
 	return 0;
 }
 
